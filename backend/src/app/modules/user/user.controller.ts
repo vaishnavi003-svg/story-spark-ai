5fe189c78d67419275422b00ab55cc0a4096d6cf
Sakshi Cholle <sakshicholle05gmail.com>
2026-05-25 16:36:56 +0530

    Fix backend controller build errors

--- backend/src/app/modules/user/user.controller.ts
+++ backend/src/app/modules/user/user.controller.ts
@@ -1,14 +1,144 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+import { UserService } from "./user.service";
+import { routeParam } from "../../../shared/route_param";
+import sendResponse from "../../../shared/send_response";
+import { getToken } from "../../middleware/token";
+import catchAsync from "../../../shared/catch_async";
+
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const result = await UserService.getAllUsers();
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "OK!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: "Fail to get users!",
+    });
+  }
+};
+
+const getUser = async (req: Request, res: Response) => {
+  try {
+    const id = routeParam(req.params.id);
+
+    const result = await UserService.getUser(id);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "OK!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: "Fail to get users!",
+    });
+  }
+};
+
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    const token = await getToken(req);
+
+    const result = await UserService.updateUser(token, req.body);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "User update successfully!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: "Fail to get users!",
+    });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const id = routeParam(req.params.id);
+
+    await UserService.deleteUser(id);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "User deleted successfully!",
+    });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: "Fail to get users!",
+    });
+  }
+};
+
+const applyForWriter = catchAsync(async (req: Request, res: Response) => {
+  const token = await getToken(req);
+
+  const result = await UserService.applyForWriter(token);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Applied for writer successfully!",
+    data: result,
+  });
+});
+
+const approveWriterApplication = catchAsync(
+  async (req: Request, res: Response) => {
+    const { email } = req.body;
+
+    const result = await UserService.approveWriterApplication(email);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Approve writer application successfully!",
+      data: result,
+    });
+  }
+);
+
+const getAllWriterApplicationUsers = catchAsync(
+  async (req: Request, res: Response) => {
+    const result = await UserService.getAllWriterApplicationUsers();
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Approve writer application successfully!",
+      data: result,
+    });
+  }
+);
+
+const getProfileInfo = catchAsync(async (req: Request, res: Response) => {
+  const token = await getToken(req);
+
+  const result = await UserService.getProfileInfo(token);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Get profile info successfully!",
+    data: result,
+  });
+});
+
 const toggleFollow = catchAsync(async (req: Request, res: Response) => {
   const token = await getToken(req);
 
-  const authorId = Array.isArray(req.params.authorId)
-    ? req.params.authorId[0]
-    : req.params.authorId;
+  const authorId = req.params.authorId as string;
 
-  const result = await UserService.toggleFollow(
-    token,
-    authorId as string
-  );
+  const result = await UserService.toggleFollow(token, authorId);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -24,13 +154,11 @@ const getFollowStatus = catchAsync(
   async (req: Request, res: Response) => {
     const token = await getToken(req);
 
-    const authorId = Array.isArray(req.params.authorId)
-      ? req.params.authorId[0]
-      : req.params.authorId;
+    const authorId = req.params.authorId as string;
 
     const result = await UserService.getFollowStatus(
       token,
-      authorId as string
+      authorId
     );
 
     sendResponse(res, {
@@ -41,3 +169,16 @@ const getFollowStatus = catchAsync(
     });
   }
 );
+
+export const UserController = {
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  getProfileInfo,
+  applyForWriter,
+  approveWriterApplication,
+  getAllWriterApplicationUsers,
+  toggleFollow,
+  getFollowStatus,
+};
